0983756e7caeb1c388cdf1333027ee9cc4e30741
sacred142 <agung.alfatah43@gmail.com>
2022-12-02 00:08:40 +0700

    menambah dashboard

--- projectPakRahmat/routes/web.php
+++ projectPakRahmat/routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
