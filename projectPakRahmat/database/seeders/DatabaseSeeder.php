dbd6db2abba83666e8191f4bfcdf5969303dbe61
sacred142 <agung.alfatah43@gmail.com>
2022-12-01 23:22:48 +0700

    menyelesaikan login

--- projectPakRahmat/database/seeders/DatabaseSeeder.php
+++ projectPakRahmat/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Agung Alfatah',
+            'username' => 'AgungA',
+            'email' => 'agung.alfatah43@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+
+        User::factory(3)->create();
     }
 }
